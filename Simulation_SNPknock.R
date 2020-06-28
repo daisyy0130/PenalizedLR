039f08114aa8358a9638a5d1566f2f089d8ac58d
Daisy Yu <67029201+daisyyu0130@users.noreply.github.com>
2020-06-27 22:15:55 -0700

    Update Simulation_SNPknock.R

--- Simulation_SNPknock.R
+++ Simulation_SNPknock.R
@@ -3,25 +3,25 @@ devtools::install_github("msesia/snpknock")
 library(dplyr)
 library(SNPknock)
 
-# Fit the Hidden Markov Model on Genotype data with fastPHASE
-# Load input genotype data on 179 CNs for 69 SNPs in NEDD9
-load("/Users/daisyyu/Desktop/Input_dataset.RData") 
-Xinp_file=writeXtoInp(X)
-fp_path="/Users/daisyyu/Desktop/Simulation/fastPHASE" 
-fp_outPath=runFastPhase(fp_path, Xinp_file, K=12, numit=30)
-r_file=paste(fp_outPath,"_rhat.txt",sep="")
-alpha_file=paste(fp_outPath,"_alphahat.txt",sep="")
-theta_file=paste(fp_outPath,"_thetahat.txt",sep="")
-char_file=paste(fp_outPath,"_origchars",sep="")
-hmm=loadHMM(r_file,alpha_file,theta_file,char_file)
+load("Input_dataset.RData") 
 
-
-simKnockoffGenotypes=function(n,Beta=log(rf(K,m,m)),hmm) {
+simKnockoffGenotypes=function(n,Beta=log(rf(K,m,m))) {
   # Input: 
   # - n is the sample size
   # - Beta is the value of log-OR simulated from log-F(m,m) distribution
-  # - hmm is the hidden Markov model contructed by fastPHASE
   # Output: sim_data
+  
+  # Fit the Hidden Markov Model on Genotype data with fastPHASE
+  # load input genotype data on 179 CNs for 69 SNPs in NEDD9
+  Xinp_file=writeXtoInp(X)
+  #fp_path="/scratch/yya188/fastPHASE" 
+  fp_path="/Users/daisyyu/Desktop/Simulation/fastPHASE"
+  fp_outPath=runFastPhase(fp_path, Xinp_file, K=12, numit=30)
+  r_file=paste(fp_outPath,"_rhat.txt",sep="")
+  alpha_file=paste(fp_outPath,"_alphahat.txt",sep="")
+  theta_file=paste(fp_outPath,"_thetahat.txt",sep="")
+  char_file=paste(fp_outPath,"_origchars",sep="")
+  hmm=loadHMM(r_file,alpha_file,theta_file,char_file)
   
   # Simulate knockoff genotypes 
   seed=sample(10000:50000,60,replace=F)
@@ -34,27 +34,27 @@ simKnockoffGenotypes=function(n,Beta=log(rf(K,m,m)),hmm) {
   # Calculate disease probability for each individual
   # choose a subset of SNPs to be casually associated with the disease
   cov_ind=sample(1:69,K,replace=F) 
+  data=data[,cov_ind]
   data$case=NULL
+  
   for (i in 1:dim(data)[1]) {
-    s=sum(data[i,cov_ind]*Beta)
-    prob=exp(-2+s)/(1+exp(-2+s))
-    if (prob >= 0.5) {data$case[i]=1}
-    else {data$case[i]=0}
+    s=sum(data[i,]*Beta)
+    prob=exp(s)/(1+exp(s))
+    data$case[i]=rbinom(1,1,prob)
   }
   
   # Sample case/control status
   # case-control ratio: 1:4
   no_case=n/5*1
   no_con=n/5*4
-  colnames(data)[1:69]=seq(1:69)
-  data_con=data %>% filter(case==0) %>% select(cov_ind,case)
-  data_case=data %>% filter(case==1) %>% select(cov_ind,case)
+  data_con=data %>% filter(case==0) 
+  data_case=data %>% filter(case==1) 
   sim_data=rbind(data_con[sample(nrow(data_con),no_con,replace=F),],
                  data_case[sample(nrow(data_case),no_case,replace=F),]) 
   colnames(sim_data)=c(paste0("x",1:K),"case");rownames(sim_data)=NULL
   return (sim_data)
 }
 
-m=2
+m=4
 K=30
-sim_data=simKnockoffGenotypes(n=200,Beta=log(rf(K,m,m)),hmm)
+sim_data=simKnockoffGenotypes(n=200,Beta=log(rf(K,m,m)))
