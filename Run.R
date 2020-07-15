50275f1ff77de1a7c1bb1be0d9ee44f4ca8217ae
Daisy Yu <67029201+daisyyu0130@users.noreply.github.com>
2020-07-15 14:37:50 -0700

    Update Run.R

--- Run.R
+++ Run.R
@@ -2,24 +2,24 @@ library(dplyr)
 library(SNPknock)
 
 load("Input_dataset.RData")
-#load("MCEM_Single-SNP.RData")
-load("MCEM_Multiple-SNP.RData")
+#load("MCEM_Multiple-SNP.RData")
+load("MCEM_Single-SNP.RData")
 
-# args <- commandArgs()
-# n <- as.numeric(args[6])
-# print(n)
+args <- commandArgs()
+n <- as.numeric(args[6])
+print(n)
 
 m=4
 K=10
-rep=10
-pp=matrix(data=NA,ncol=rep,nrow=20)
-for (i in 1:rep) {
-  #set.seed(2000*n+i+K)
-  sim_data=simKnockoffGenotypes(n=200,Beta=log(rf(K,m,m)))
-  pp[,i]=profilelkhd(data=sim_data,mvals=c(1:20),N=1000)
-}
+set.seed(2020*n+K)
+#sim_data=simUnmatched(n=200,Beta=log(rf(K,m,m)),p=0)     # continuous data
+sim_data=simKnockoffGenotypes(n=200,Beta=log(rf(K,m,m)))  # SNP data
+pp=profilelkhd(data=sim_data,mvals=c(1:20),N=1000)
 
-# save(pp,file=paste0("K=50_rep",n,".RData"))
+save(pp,file=paste0("K=10_rep",n,".RData"))
+
+
+
 
 
 
